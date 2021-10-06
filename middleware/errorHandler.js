8d4047b6e4378afa9821be08453da1bde6dca2f9
Moritz Miedler <71124607+MoritzMiedler@users.noreply.github.com>
2021-10-06 19:01:15 +0200

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,13 @@
-/* eslint no-unused-vars: off */
+const errorHandler = (err, req, res) => {
+  console.log('Error => ', err.message.red);
+  res.status(500).json({
+    data: 'Server error',
+  });
+};
 
-const errorHandler = (err, req, res, next) => {};
-const notFound = (req, res) => {};
-
-module.exports = { errorHandler, notFound };
+const notFoundHandler = (req, res) => {
+  console.log('Error => ', req.originalUrl.blue);
+  res.status(404).json({
+    data: 'The requested page does not exist.',
+  });
+};
